990daed92d5f58be47e1e7d7029a2a358ae057b4
Garcia Cynthya <cyngarcia25@gmail.com>
2022-08-15 00:16:42 -0300

    agrego las consultas

---
+++ cosultas.sql
@@ -0,0 +1,8 @@
+---Agrego consultas generadas
+---punto 6
+select * from historial WHERE descripcion = 'peluqueria' and fecha = '01-01-2022'
+
+---punto 9
+update dueño
+             set direccion = 'libertad 123'
+             WHERE dni_dueño = 41562234
